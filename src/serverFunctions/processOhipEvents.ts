4b80866d542ed26603e87d23ab69d083b351b33d
Danijel Dragicevic <danijel.dragicevic@productdock.com>
2026-03-19 14:19:57 +0100

    change context for the server fx

--- src/serverFunctions/processOhipEvents.ts
+++ src/serverFunctions/processOhipEvents.ts
@@ -1,8 +1,8 @@
-import poly, { PolyServerFunction } from "polyapi";
+import { PolyServerFunction } from "polyapi";
 
 // PolyAPI configuratoin
 export const polyConfig: PolyServerFunction = {
-    context: "demo.ohip",
+    context: "ohip",
     name: "processOhipEvents",
     description: "Process incoming OHIP events and send update to the Zoom API.",
     visibility: "TENANT",
@@ -13,7 +13,7 @@ export const polyConfig: PolyServerFunction = {
 /**
  * Process incoming OHIP events and send update to the Zoom API.
  * @param {any} eventPayload - The raw incoming OHIP webhook event payload to ingest. Contains the event type/notification (e.g., patient check-in), associated timestamps, patient and clinic metadata, and any identifiers needed to log the event for auditing/troubleshooting and to forward corresponding status updates to the Zoom API.
- * @returns {Promise<void>} 
+ * @returns {Promise<void>}
  */
 export async function processOhipEvents(eventPayload: any): Promise<void> {
     console.log("Received OHIP event:", eventPayload);
